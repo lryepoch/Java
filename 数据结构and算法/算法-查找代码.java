1662dab80d612325c2edae05beb0a3e2c512f80a
lryepoch <1783510791@qq.com>
2021-02-25 17:31:17 +0800

    2021-2-25

--- 数据结构and算法/算法-查找代码.java
+++ 数据结构and算法/算法-查找代码.java
@@ -1,12 +1,14 @@
 【线性表】
 【树表】
 【散列表】
---------------------------
-线性表的查找：
-//顺序查找
-//如果找到，返回数组下标；
-//时间复杂度O(n), 空间复杂度O(1)
 
+线性表
+--------------------------
+
+//顺序查找
+//如果找到，返回数组下标
+//时间复杂度O(n)
+//空间复杂度O(1)
 class Solution{
 	public int seqSearch(int[] a,int key){
 		for (int i=0; i<a.length; i++){
@@ -15,19 +17,13 @@ class Solution{
 		}
 		return -1;
 	}
-	public static void main(String args[]){
-
-		int a[]={5,6,8,2,4,0};
-		Solution s = new Solution();
-		int k = s.SeqSearch(a,0);
-		System.out.println("下标="+k);
-	}
 }
 
 //折半查找, 又叫二分查找。
-//前提条件是需要有序表顺序存储;对于静态查找表，一次排序后不再变化，折半查找能得到不错的效率。但对于需要频繁执行插入或删除操作的数据集来说，维护有序的排序会带来不小的工作量，那就不建议使用。
+//前提条件：需要有序表顺序存储;
 //适用于不经常变动而查找频繁的有序列表；
-//时间复杂度是O(logn)
+//时间复杂度O(logn)
+//空间复杂度O()
 
 **递归实现**
 //使用递归时，方法参数需要携带诸如 left，right；start，end；low，high；
@@ -61,26 +57,23 @@ class Solution{
 		}
 		return -1;
 	}
-	public static void main(String args[]){
-
-		int a[]={1,3,4,5,7,9};
-		Solution s = new Solution();
-		int k = s.Search_Bin(a,7);
-		System.out.println("下标="+k);
-
-	}
 }
 
 
-1.在等概率下，查找成功时的平均查找长度相同，查找失败时的平均查找长度不相同。
 
 
+
+
+
+
+树表
 ----------------------
-树表的查找：
 
 
 
 
 
+
+
+散列表
 ---------------------
-散列表的查找：
